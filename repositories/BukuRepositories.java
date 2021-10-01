abb2032f49f7d8b39ba90687f91bb0a1d82c0be8
ubaidahh <90589123+ubaidahh@users.noreply.github.com>
2021-10-01 21:10:12 +0700

    Delete repositories directory

--- repositories/BukuRepositories.java
+++
@@ -1,9 +0,0 @@
-package com.perpustakaan.projekperpustakaan.repositories;
-
-import com.perpustakaan.projekperpustakaan.models.Buku;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BukuRepositories extends JpaRepository<Buku,String> {
-}
