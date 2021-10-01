b98a692cb425aae1afec9a5685b4fa44282505f6
ubaidahh <90589123+ubaidahh@users.noreply.github.com>
2021-10-01 21:11:37 +0700

    halaman utama di web

---
+++ home.php
@@ -0,0 +1,36 @@
+<marquee>Selamat Datang di Perpustakaan Team Wan, Jangan Macem-macem Bisa Kena Marah Lho!</marquee>            
+            <div id="page-inner">
+                <div class="row">
+                    <div class="col-md-12">
+                     <h2>Perpustakaan Team Wan</h2>   
+                        <h5></h5>
+                    </div>
+                </div>              
+                 <!-- /. ROW  -->
+                 <hr />
+                <div class="row">
+                 <div class="col-md-3 col-sm-12 col-xs-12">                       
+                    <div class="panel panel-primary text-center no-boder bg-color-green">
+                        <div class="panel-body">
+                            <i class="fa fa-users fa-5x"></i>
+                            <h3>Data Peminjam </h3>
+                        </div>
+                        <div class="panel-footer back-footer-green">
+                       
+                        </div>
+                    </div>
+                    <div class="panel panel-primary text-center no-boder bg-color-red">
+                        <div class="panel-body">
+                            <i class="fa fa-edit fa-5x"></i>
+                            <h3>Data Buku </h3>
+                        </div>
+                        <div class="panel-footer back-footer-red">
+                            
+                            
+                        </div>
+                    </div>                         
+                        </div>
+                                        
+             </div>
+		     </div>
+			</div>
